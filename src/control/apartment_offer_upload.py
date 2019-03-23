ac5e3f2ae24445f8cf7b427316710c50bbf88155
Andres Lopez Martinez <andoresu@ciencias.unam.mx>
2019-03-23 16:22:11 +0100

    Actually implemented calls to slack notification functionality...

--- src/control/apartment_offer_upload.py
+++ src/control/apartment_offer_upload.py
@@ -1,11 +1,16 @@
 import src.control.functions as fn
 
 if __name__ == '__main__':
-    # Current offer size
-    db_size = fn.get_db_offering_size()
-    live_size = fn.get_live_offering_size()
+    try:
+        # Current offer size
+        db_size = fn.get_db_offering_size()
+        live_size = fn.get_live_offering_size()
 
-    # Check if any new apartment has been posted
-    if live_size != db_size:
-        fn.scrape_apartments()
-        fn.scrape_offering()
+        # Check if any new apartment has been posted
+        if live_size != db_size:
+            fn.scrape_apartments()
+            fn.scrape_offering()
+        fn.send_slack_notification(error=False)
+
+    except Exception as e:
+        fn.send_slack_notification(error=True)
--- src/control/apartment_state_upload.py
+++ src/control/apartment_state_upload.py
@@ -2,16 +2,22 @@ import datetime
 import src.control.functions as fn
 
 if __name__ == '__main__':
-    # Current offer timestamps
-    start_date, end_date = fn.get_last_offer_timestamps()
-    timezone = start_date.tzinfo
-    current_timestamp = datetime.datetime.now()
-    current_timestamp = current_timestamp.replace(tzinfo=timezone)
+    try:
+        # Current offer timestamps
+        start_date, end_date = fn.get_last_offer_timestamps()
+        timezone = start_date.tzinfo
+        current_timestamp = datetime.datetime.now()
+        current_timestamp = current_timestamp.replace(tzinfo=timezone)
 
-    if start_date <= current_timestamp <= end_date:
-        fn.scrape_apartment_states()
-    elif end_date < current_timestamp:
-        fn.scrape_apartments()
-        fn.scrape_offering()
-    else:
-        raise Exception("Error in timestamps")
+        if start_date <= current_timestamp <= end_date:
+            fn.scrape_apartment_states()
+        elif end_date < current_timestamp:
+            fn.scrape_apartments()
+            fn.scrape_offering()
+        else:
+            raise Exception("Error in timestamps")
+
+        fn.send_slack_notification(error=False)
+
+    except Exception as e:
+        fn.send_slack_notification(error=True)
